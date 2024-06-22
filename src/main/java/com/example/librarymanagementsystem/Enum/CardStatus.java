389c71522e733a1fcd1e20fe31d6138905ead804
MdAzarudeen <mohazar671@gmail.com>
2024-06-22 10:39:04 +0530

    commit 3  project

--- src/main/java/com/example/librarymanagementsystem/Enum/CardStatus.java
+++ src/main/java/com/example/librarymanagementsystem/Enum/CardStatus.java
@@ -3,6 +3,7 @@ package com.example.librarymanagementsystem.Enum;
 public enum CardStatus {
     Active,
     NEW,
+    OLD,
     EXPIRED,
     DEACTIVATED,
     BLOCKED
--- src/test/java/com/example/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
+++ src/test/java/com/example/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.library.management.system;
+package com.example.librarymanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
